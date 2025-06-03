8cb6df96472560054ecdc84eacf34a1e1138e731
Oliver <softwaredevdad@gmail.com>
2025-06-03 19:18:19 +0200

    Config Update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,20 +1,54 @@
 return {
-  "zenbones-theme/zenbones.nvim",
-  -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-  -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-  -- In Vim, compat mode is turned on as Lush only works in Neovim.
-  dependencies = "rktjmp/lush.nvim",
-
-  priority = 1000,
-  lazy = false,
-  -- you can set set configuration options here
-  config = function()
-    vim.g.zenbones_darken_comments = 45
-    vim.g.zenbones_compat = 0
-    vim.cmd.colorscheme("zenbones")
-  end,
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+      -- Default options:
+      require("gruvbox").setup({
+        terminal_colors = true, -- add neovim terminal colors
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = true,
+          emphasis = true,
+          comments = true,
+          operators = false,
+          folds = true,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        contrast = "", -- can be "hard", "soft" or empty string
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = false,
+      })
+      vim.cmd("colorscheme gruvbox")
+    end,
+  },
 }
 
+-- return {
+--   "zenbones-theme/zenbones.nvim",
+--   -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+--   -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+--   -- In Vim, compat mode is turned on as Lush only works in Neovim.
+--   dependencies = "rktjmp/lush.nvim",
+--
+--   priority = 1000,
+--   lazy = false,
+--   -- you can set set configuration options here
+--   config = function()
+--     vim.g.zenbones_darken_comments = 45
+--     vim.g.zenbones_compat = 0
+--     vim.cmd.colorscheme("zenbones")
+--   end,
+-- }
+--
 -- return {
 --   {
 --     "anAcc22/sakura.nvim",
